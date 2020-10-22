2a809e0098145078dd7a6e1e36640c9f2bb7a5fc
Davin Wijaya <wijayadavin@gmail.com>
2020-10-22 19:01:38 +0700

    add fromHeaderOrQuerystring(req) feature

--- middleware/authorizationMiddleware.js
+++ middleware/authorizationMiddleware.js
@@ -1,11 +1,19 @@
 const jwt = require("jsonwebtoken")
 const jwtConfigurations = require("../config/jwtConfigurations")
 
+function fromHeaderOrQuerystring(req) {
+    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+        return req.headers.authorization.split(' ')[1];
+    } else if (req.query && req.query.token) {
+        return req.query.token;
+    }
+    return null;
+}
 
 function authorization(req, res, next) {
-    const token = req.headers.authorization
+    const token = fromHeaderOrQuerystring(req)
     if (!token) {
-        res.status(401).send("Unauthorized")
+        res.status(401).send("Error: access denied")
     } else {
         // JWT validation:
         const tokenBody = token.slice(7)

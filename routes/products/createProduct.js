db8b79236ca27dd6edda9b1c682b976cd9ff3505
Davin Wijaya <wijayadavin@gmail.com>
2020-10-22 23:11:13 +0700

    develop CRUD for Products

---
+++ routes/products/createProduct.js
@@ -0,0 +1,18 @@
+/* eslint-disable new-cap */
+const express = require('express');
+const app = express.Router();
+const db = require('../../controller/dbController');
+
+app.post('/products', (req, res) => {
+  const result = db.add('products', req.body);
+  console.log(result);
+  if (!result) {
+    res.status(400).send('Wrong body');
+  } else {
+    res.send(result);
+  }
+  return;
+});
+
+
+module.exports = app;
---
+++ routes/products/readProduct.js
@@ -0,0 +1,19 @@
+/* eslint-disable new-cap */
+const express = require('express');
+const app = express.Router();
+const db = require('../controller/dbController');
+
+
+app.post('/products', (req, res) => {
+  const result = db.get('products', req.body);
+  console.log(result);
+  if (!result) {
+    res.status(400).send('Wrong body');
+  } else {
+    res.send(result);
+  }
+  return;
+});
+
+
+module.exports = app;

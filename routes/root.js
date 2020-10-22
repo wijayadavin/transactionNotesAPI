304d35875e5efca046855ed68b104ed4a535b0a2
Davin Wijaya <wijayadavin@gmail.com>
2020-10-22 18:00:50 +0700

    testing JWT token

--- routes/root.js
+++ routes/root.js
@@ -1,9 +1,18 @@
 const express = require("express")
 const app = express.Router()
+const jwt = require('jsonwebtoken')
 
 
 app.get("/", (req, res) => {
-  res.send("Hello world!")
+  // Set a payload
+  const payload = {
+    name: "Davin",
+    scopes: ["transaction: create"]
+  }
+
+  // Generate a token according to the secret key:
+  const token = jwt.sign(payload, "Secret key")
+  res.send(token)
 })
 
 
--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const rootRoute = require("./routes/root")
 
-const getAccount = require("./routes/accounts/getAccount")
-const addTransactions = require("./routes/transactions/addTransactions")
-const getTransactions = require("./routes/transactions/getTransactions")
 const signup = require("./routes/signup")
 const login = require("./routes/login")
 
@@ -12,9 +9,6 @@ const app = express()
 
 app.use(bodyParser.json())
 app.use(rootRoute)
-app.use(getAccount)
-app.use(addTransactions)
-app.use(getTransactions)
 app.use(signup)
 app.use(login)
 
